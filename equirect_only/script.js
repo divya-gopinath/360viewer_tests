2ba937e5eab227351ef7558ea08b1a7d02066075
Divya Gopinath <divya@LAPTOP-27SB9AS3.mit.edu>
2019-04-12 00:05:24 -0400

    Calculate spherical coords from equirectangular

--- equirect_only/script.js
+++ equirect_only/script.js
@@ -1,10 +1,23 @@
+var localizedSources = [];
+
 $( document ).ready(function() {
   $("#video-box").click(function(e) {
     e.preventDefault();
     var offset = $(this).offset();
-    var relativeX = (e.pageX - offset.left);
-    var relativeY = (e.pageY - offset.top);
+    var width = $(this).width();
+    var height = $(this).height();
+    var relativeX = (e.pageX - offset.left)/width;
+    var relativeY = (e.pageY - offset.top)/height;
+    var  time = $(this)[0].currentTime;
+    var datapoint = {
+      "time" : time,
+      "x" : relativeX,
+      "y" : relativeY,
+      "theta" : 2 * Math.PI * relativeX,
+      "phi" : Math.PI * relativeY
+    };
     alert("Sound localized at \n X: " + relativeX + "  Y: " + relativeY);
+    localizedSources.push(datapoint);
   });
 });
 
